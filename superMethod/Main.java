61a8273d7685c949f18fb310cb27a7df7df57a0d
João <joao7vieira@gmail.com>
2025-10-19 16:47:32 +0100

    Part5

---
+++ superMethod/Main.java
@@ -0,0 +1,31 @@
+package superMethod;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+
+
+        // super = Refers to the parent class (subclass <- superclass)
+
+        //               Used in constructors and method overriding
+
+        //               Calls the parent constructor to initialize attributes
+
+
+
+        Person person = new Person("Tom", "Riddle");
+
+        Student student = new Student("Harry", "Potter", 3.25);
+
+        Employee employee = new Employee("Rubeus", "Hagrid", 50000);
+
+        student.showName();
+        student.showGPA();
+        employee.showName();
+        employee.showSalary();
+        person.showName();
+
+    }
+
+}
---
+++ superMethod/Student.java
@@ -0,0 +1,27 @@
+package superMethod;
+
+public class Student extends Person{
+
+
+
+    double gpa;
+
+
+
+    Student(String first, String last, double gpa){
+
+        super(first, last);
+
+        this.gpa = gpa;
+
+    }
+
+
+
+    void showGPA(){
+
+        System.out.println(this.first + "'s gpa is: " + this.gpa);
+
+    }
+
+}
