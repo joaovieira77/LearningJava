f16b0c5eeaa387d422e95b836b9280c8f2bbdf38
João <joao7vieira@gmail.com>
2025-10-19 19:49:55 +0100

    pt8

---
+++ composition/Engine.java
@@ -0,0 +1,25 @@
+package composition;
+
+public class Engine {
+
+
+
+    String type;
+
+
+
+    Engine(String type){
+
+        this.type = type;
+
+    }
+
+
+
+    void start(){
+
+        System.out.println("You start the " + this.type + " engine");
+
+    }
+
+}
