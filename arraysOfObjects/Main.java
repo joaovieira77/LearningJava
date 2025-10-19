df993b70864fae3f0438c9490209b2a8f8a38fba
João <joao7vieira@gmail.com>
2025-10-19 15:31:10 +0100

    Part 4 commit 4, continuing on POO

---
+++ arraysOfObjects/Main.java
@@ -0,0 +1,14 @@
+package arraysOfObjects;
+
+public class Main {
+        public static void main(String[] args) {
+           
+
+            Car[] cars= {new Car("Ferrari", "Red"),
+                         new Car("Porsche", "Black"),
+                         new Car("Mercedes", "Gray")};
+            for (Car car: cars){
+                car.drive();
+            }
+        }
+}
