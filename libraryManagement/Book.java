076ff2af50b40194685a23ad5c0ef2e511d22cec
João <joao7vieira@gmail.com>
2025-10-22 18:04:19 +0100

    Book and magazine added

--- libraryManagement/Book.java
+++ libraryManagement/Book.java
@@ -1,5 +1,15 @@
 package libraryManagement;
 
-public class Book {
-    
+public class Book extends LibraryItem {
+    private String author;
+
+    public Book(String title, String id, String author) {
+        super(title, id);
+        this.author = author;
+    }
+
+    @Override
+    public void displayInfo() {
+        System.out.println("Book: " + title + " | ID: " + id + " | Author: " + author);
+    }
 }
--- libraryManagement/Magazine.java
+++ libraryManagement/Magazine.java
@@ -1,5 +1,15 @@
 package libraryManagement;
 
-public class Magazine {
-    
+public class Magazine extends LibraryItem {
+    private int issueNumber;
+
+    public Magazine(String title, String id, int issueNumber) {
+        super(title, id);
+        this.issueNumber = issueNumber;
+    }
+
+    @Override
+    public void displayInfo() {
+        System.out.println("Magazine: " + title + " | ID: " + id + " | Issue: " + issueNumber);
+    }
 }
