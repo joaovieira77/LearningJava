1543f7076d3508ab8d9ac8f6a96a198a05386566
João <joao7vieira@gmail.com>
2025-10-23 00:26:36 +0100

    Add abstract Person class with name and ID fields

--- GradebookSystem/Person.java
+++ GradebookSystem/Person.java
@@ -1,5 +1,19 @@
 package GradebookSystem;
 
 public class Person {
-    
+    protected String name;
+    protected String id;
+
+    public Person(String name, String id) {
+        this.name = name;
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getId() {
+        return id;
+    }
 }
