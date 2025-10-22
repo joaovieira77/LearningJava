85099f5f5e01e94739adfa2e4e393ae4e7303f2e
João <joao7vieira@gmail.com>
2025-10-23 00:27:51 +0100

    Add Student class extending Person with subject list and grade methods Add Subject class to represent subject name and grade

--- GradebookSystem/Student.java
+++ GradebookSystem/Student.java
@@ -1,5 +1,31 @@
 package GradebookSystem;
 
-public class Student {
-    
+import java.util.ArrayList;
+
+public class Student extends Person {
+    private ArrayList<Subject> subjects = new ArrayList<>();
+
+    public Student(String name, String id) {
+        super(name, id);
+    }
+
+    public void addSubject(Subject subject) {
+        subjects.add(subject);
+    }
+
+    public void displayGrades() {
+        System.out.println(" Grades for " + name + ":");
+        for (Subject s : subjects) {
+            System.out.println("- " + s.getName() + ": " + s.getGrade());
+        }
+    }
+
+    public double getAverageGrade() {
+        if (subjects.isEmpty()) return 0;
+        double total = 0;
+        for (Subject s : subjects) {
+            total += s.getGrade();
+        }
+        return total / subjects.size();
+    }
 }
--- GradebookSystem/Subject.java
+++ GradebookSystem/Subject.java
@@ -1,5 +1,19 @@
 package GradebookSystem;
 
 public class Subject {
-    
+    private String name;
+    private double grade;
+
+    public Subject(String name, double grade) {
+        this.name = name;
+        this.grade = grade;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getGrade() {
+        return grade;
+    }
 }
