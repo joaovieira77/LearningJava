2334d028e556f3e5898340215104e0f82c919b89
João <joao7vieira@gmail.com>
2025-10-22 18:03:28 +0100

    LibraryItem added

--- libraryManagement/LibraryItem.java
+++ libraryManagement/LibraryItem.java
@@ -1,5 +1,21 @@
 package libraryManagement;
 
-public class LibraryItem {
-    
+public abstract class LibraryItem {
+    protected String title;
+    protected String id;
+
+    public LibraryItem(String title, String id) {
+        this.title = title;
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public abstract void displayInfo();
 }
